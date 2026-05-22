f60b095fa3d0b7136f2f4e72a5018201a4921aca
osyau <os.yau2701@gmail.com>
2026-05-22 00:48:25 -0500

    main actualizado

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 def init_project ():
     """Función para inicializar el proyecto, configurando el entorno y preparando los recursos necesarios."""
     print ("[INFO] Inicializando el proyecto...")
-    # Aquí irá más adelante la lectura de archivos o conexiones a bases de datos
+    # Aquí irá más adelante la lectura de archivos/conexiones database
     return True
 
 def main():
@@ -11,7 +11,7 @@ def main():
     print("=========================================")  
     if init_project():
         print("[INFO] proyecto inicializado correctamente.")
-        # aqui ira el nucleo de la aplicación, como la interfaz de usuario o el procesamiento de datos
+        # aqui ira el nucleo de la aplicación, como: interfaz de usuario o procesamiento de datos
     else:
         print("[ERROR] No se pudo inicializar el proyceto.")
         sys.exit(1)
